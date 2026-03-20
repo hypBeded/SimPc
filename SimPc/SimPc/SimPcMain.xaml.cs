e82bb82b9bb0e9fe9b598ba875886e68d6ddb290
HypBed <uymaevymat@gmail.com>
2026-03-20 11:03:32 +0300

    Интерфейс

---
+++ SimPc/SimPc/SimPcMain.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace SimPc
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+        public partial class MainWindow : Window
+        {
+        
+
+
+
+        }
+    }
