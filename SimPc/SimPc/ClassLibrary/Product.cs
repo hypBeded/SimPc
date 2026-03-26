6c89d1fe20a16f030a5f05ac655b0e2c076c2e6e
Умар Юмаев <uymaevymat@gmail.com>
2026-03-26 12:21:15 +0300

    Добавлены класс DbHelper и Products

---
+++ SimPc/SimPc/ClassLibrary/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimPc.ClassLibrary
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+        public string Manufacturer { get; set; }
+        public bool IsInBuild { get; set; } 
+
+        public Product()
+        {
+            Name = string.Empty;
+            Description = string.Empty;
+            Category = string.Empty;
+            Manufacturer = string.Empty;
+        }
+    }
+}
--- SimPc/SimPc/SimPcMain.xaml.cs
+++ SimPc/SimPc/SimPcMain.xaml.cs
@@ -1,25 +1,26 @@
-﻿using System;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Text.Json;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
+﻿using System.Windows;
+using SimPc.ClassLibrary; 
 
 namespace SimPc
 {
+
+
+   
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-        public partial class MainWindow : Window
+    public partial class SimPcMain : Window
+    {
+        public SimPcMain()
         {
-        
-
-
-
+            InitializeComponent();
         }
+
+       
+
+
+
+
     }
+}
+    
